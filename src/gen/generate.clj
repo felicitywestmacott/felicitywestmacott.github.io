1e4bf0744dae65d183134a8a86dac050094d0cc9
Peter Westmacott <peter.westmacott@plumbee.co.uk>
2016-03-17 15:53:57 +0000

    Test generate one set of client pages.

--- src/gen/generate.clj
+++ src/gen/generate.clj
@@ -3,6 +3,7 @@
 
 
 (def known-filter-categories ["Theme" "Category" "Detail" "Colour" "Fabric"])
+(def numbers-from-one (iterate inc 1))
 
 (def filters (edn/read-string (slurp "./resources/filters.edn")))
 (def portfolio (edn/read-string (slurp "./resources/portfolio.edn")))
@@ -52,3 +53,45 @@
 (def portfolio-yaml (reduce build-portfolio-yaml "" portfolio))
 
 (spit "./_data/portfolio.yml" portfolio-yaml)
+
+
+
+(defn pic-name [pic] (.replace (:image pic) ".jpg" ""))
+
+(defn find-next [pics current]
+  (first (filter #(= current (:prevpic %)) pics)))
+
+(defn pics-chain [pic pics]
+  (if (nil? (:nextpic pic))
+    [pic]
+    (let [next (find-next pics (pic-name pic))]
+      (concat [pic] (pics-chain next pics)))))
+
+(defn sorted-pics [pics]
+  (let [first-pic (find-next pics nil)]
+    (pics-chain first-pic pics)))
+
+
+
+(defn build-client-page [title client-blurb total counter {:keys [image nextpic prevpic blurb]}]
+  (let [content (str "---\n"
+                     "layout: client\n"
+                     "title: " title "\n"
+                     "image: " image "\n"
+                     (if nextpic (str "nextpic: " nextpic "\n") "\n")
+                     (if prevpic (str "prevpic: " prevpic "\n") "\n")
+                     "counter: " counter " / " total "\n"
+                     "---\n"
+                     "\n"
+                     client-blurb
+                     "\n"
+                     blurb)
+        filename (str "./portfolio/" (.replace image ".jpg" ".html"))]
+    (spit filename content)))
+
+(defn build-client-pages [{:keys [title pics blurb]}]
+  (let [builder (partial build-client-page title blurb (count pics))]
+    (mapv builder numbers-from-one (sorted-pics pics))))
+
+(mapv build-client-pages (take 1 (vals clients)))
+
